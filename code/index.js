97d9d6e494bad565c80076f2feb4d819b6a0fa9e
riven78 <riven78@163.com>
2019-08-21 17:11:33 +0800

    1.0.1版本发布

--- code/index.js
+++ code/index.js
@@ -2,31 +2,33 @@ const http = url = require("url"),
   path = require("path"),
   child_process = require('child_process');
 
-class shutdownpc {
-  execute(req, res) {
-    var pathname = url.parse(req.url).pathname;
-    if (path.basename(pathname) == "shutdownpc") {
-      var workerProcess = child_process.exec("shutdown.exe -s -t 30", function (error, stdout, stderr) {
-        res.writeHead(200, {"Content-Type": "text/plain"});
-        var errStr = "";
-        if (error) {
-          errStr += "\n" + error.stack;
-          errStr += "\n" + 'Error code: ' + error.code;
-          errStr += "\n" + 'Signal received: ' + error.signal;
-          console.log(error.stack);
-          console.log('Error code: ' + error.code);
-          console.log('Signal received: ' + error.signal);
-        }
-        console.log('stdout: ' + stdout);
-        console.log('stderr: ' + stderr);
+const execute = function (req, res) {
+  var pathname = url.parse(req.url).pathname;
+  if (path.basename(pathname) == "shutdownpc") {
+    var workerProcess = child_process.exec("shutdown.exe -s -t 30", function (error, stdout, stderr) {
+      res.writeHead(200, {"Content-Type": "text/plain"});
+      var errStr = "";
+      if (error) {
+        errStr += "\n" + error.stack;
+        errStr += "\n" + 'Error code: ' + error.code;
+        errStr += "\n" + 'Signal received: ' + error.signal;
+        console.log(error.stack);
+        console.log('Error code: ' + error.code);
+        console.log('Signal received: ' + error.signal);
+      }
+      console.log('stdout: ' + stdout);
+      console.log('stderr: ' + stderr);
 
-        errStr += "\n" + 'stdout: ' + stdout;
-        errStr += "\n" + 'stderr: ' + stderr;
-        res.end(errStr);
-      });
-      workerProcess.on('exit', function (code) {
-        console.log('子进程已退出，退出码 ' + code);
-      });
-    }
+      errStr += "\n" + 'stdout: ' + stdout;
+      errStr += "\n" + 'stderr: ' + stderr;
+      res.end(errStr);
+    });
+    workerProcess.on('exit', function (code) {
+      console.log('子进程已退出，退出码 ' + code);
+    });
   }
-}
+};
+
+module.exports = {
+  execute: (req, res) => execute(req, res)
+};
